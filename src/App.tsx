9e22f966e4d7386829d96a4ec2edbbd5d3543f76
mark-khodunov <mark.khodunov@yandex.ru>
2025-06-13 17:19:56 +1000

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class="container">
+        <div class="container1">
+
+          <div class="base2">
+
+            <div class="container02"><img src="./src/imgs/7-01.svg" alt="" /></div>
+            
+            <img class="container2" src="logo.png" alt="" />
+
+            <div class="container3">
+
+              <div class="container03">
+                <img src="./src/imgs/1.svg" alt="" />
+              </div>
+
+              <div class="container03">
+                <img src="./src/imgs/2.svg" alt="" />
+              </div>
+
+              <div class="container03">
+                <img src="./src/imgs/3.svg" alt="" />
+              </div>
+
+              <div class="container03">
+                <img src="./src/imgs/4.svg" alt="" />
+              </div>
+
+              <div class="container03">
+                <img src="./src/imgs/5.svg" alt="" />
+              </div>
+
+              <div class="container03">
+                <img src="./src/imgs/6.svg" alt="" />
+              </div>
+
+            </div>
+
+            <div class="container4">
+              <div class="container04">
+                <img src="./src/imgs/3.svg" alt="" />
+              </div>
+              <div class="container04">
+                <img src="./src/imgs/TV1.svg" alt="" />
+              </div>
+            </div>
+
+            <div class="container5"><img src="1.svg" alt="" /></div>
+
+          </div>
+
+          <div class="base1">
+
+            <div class="container8">Technifly</div>
+
+            <div class="container9">
+              <div class="container09">Dashboard</div>
+              <div class="container09">Sales</div>
+              <div class="container09">Costs</div>
+              <div class="container09">Payments</div>
+              <div class="container09">Finances</div>
+              <div class="container09">Messages</div>
+            </div>
+
+            <div class="container10">
+              <div class="container101">Settings</div>
+              <div class="container1011">Support</div>
+            </div>
+
+          </div>
+
+          <div class="base">
+
+            <div class="container6"><img src="Strelka1-01.svg" alt="" /></div>
+
+            <div class="container7"><img src="Strelki-01.svg" alt="" /></div>
+
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
